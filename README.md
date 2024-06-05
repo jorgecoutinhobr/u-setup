179bb6c3247879c263e65e80b7579b0d43957f91
Jorge Coutinho <jorgecsn@id.uff.br>
2024-06-05 20:51:06 -0300

    Update dependencies and install new packages

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# dev-setup-ubuntu-20-04
+# dev-setup-ubuntu-24-04
+
+## Scripts
+- [`dependencies.sh`](dependencies.sh): This script makes it easier to install the programs I use in my development environment
+
+- [`step_2.sh`](step_2.sh): This script installs and configures Ruby and Node.js using asdf, and also installs Rails and Yarn
+
+## Usage
+1. Clone this repository to your computer.
+2. Make the scripts executable with the command `chmod +x dependencies.sh step_2.sh`.
+3. Run the `dependencies.sh` script to install the dependencies: `./dependencies.sh`.
+4. Run the `step_2.sh` script to install and configure Ruby and Node.js: `./step_2.sh`.
+
+## TODO
+- add agnoster to oh my bash theme
+- FIX notion
+- add mysql and postgre?
--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
 
-sh -c "$(wget https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # install asdf
 git clone https://github.com/excid3/asdf.git ~/.asdf
@@ -13,14 +13,38 @@ echo 'export EDITOR="code --wait"' >> ~/.bashrc
 exec $SHELL
 
 # Installl Opera
-# wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
-# echo "deb https://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list
-# sudo apt-get update && sudo apt-get install opera-stable
-
-# Install Notion
-# wget https://www.notion.so/desktop/linux/download -O notion.deb
-# sudo dpkg -i notion.deb
-# sudo apt-get install -f
+wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+echo "deb https://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list
+sudo apt-get update && sudo apt-get install opera-stable
 
 # install code
-# sudo apt-get install code
+sudo apt-get install code
+
+# install flameshot
+sudo apt install flameshot
+
+# install openfortiGUI
+wget https://apt.iteas.at/iteas/pool/main/o/openfortigui/openfortigui_0.9.10-1_amd64_noble.deb -O openfortigui.deb
+sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qt5keychain-dev libqt5core5a libqt5gui5 libqt5keychain1 libqt5network5 libqt5widgets5 qttranslations5-l10n ppp
+sudo apt-get install -f
+sudo dpkg -i openfortigui.deb
+
+# install chromium
+sudo apt install chromium-chromedriver
+
+# install chrome 117
+wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_117.0.5938.132-1_amd64.deb
+sudo apt-get install libasound2t64
+sudo apt-get install fonts-liberation libasound2 libnspr4 libnss3 libu2f-udev xdg-utils
+#sudo apt-get install -f
+sudo dpkg -i google-chrome-stable_117.0.5938.132-1_amd64.deb
+sudo apt-mark hold google-chrome-stable # prevent auto update
+
+# install notion - NEED TO FIX  
+# sudo apt install liblua5.3-0
+# wget http://archive.ubuntu.com/ubuntu/pool/multiverse/n/notion/notion_4.0.2+dfsg-7build1_amd64.deb -O notion.deb
+# sudo apt install ./notion.deb
+
+# install discord
+wget "https://discordapp.com/api/download?platform=linux&format=deb" -O discord.deb 
+sudo apt install ./discord.deb
