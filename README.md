162f925ce8fe203b9f5a1067a4f8184fd1d0a7e6
Jorge Coutinho <jorgecsn@id.uff.br>
2024-06-05 21:48:58 -0300

    some fixs

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # dev-setup-ubuntu-24-04
 
 ## Scripts
-- [`dependencies.sh`](dependencies.sh): This script makes it easier to install the programs I use in my development environment
-
+- [`step_1.sh`](step_1.sh): This script install dependencies and oh my bash
+- [`step_2.sh`](step_2.sh): This script makes it easier to install the programs I use in my development environment
 - [`step_2.sh`](step_2.sh): This script installs and configures Ruby and Node.js using asdf, and also installs Rails and Yarn
 
 ## Usage
 1. Clone this repository to your computer.
-2. Make the scripts executable with the command `chmod +x dependencies.sh step_2.sh`.
-3. Run the `dependencies.sh` script to install the dependencies: `./dependencies.sh`.
-4. Run the `step_2.sh` script to install and configure Ruby and Node.js: `./step_2.sh`.
+2. Make the scripts executable with the command `chmod +x step_1.sh step_2.sh step_3.sh`
+3. Run the `step_1.sh` script to install dependencies and oh ma bash: `./step_1.sh`.
+4. Run the `step_2.sh` script to install some softwares: `./step_2.sh`.
+5. Close the terminal and open it again
+6. Run the `step_3.sh` script to install and configure Ruby and Node.js: `./step_3.sh`.
 
 ## TODO
 - add agnoster to oh my bash theme
 - FIX notion
 - add mysql and postgre?
+- find a way to do it all at once
--- step_2.sh
+++ step_2.sh
@@ -1,14 +1,43 @@
-# Add asdf plugins
-asdf plugin add ruby
-asdf plugin add nodejs
+# install asdf
+git clone https://github.com/excid3/asdf.git ~/.asdf
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
+echo 'legacy_version_file = yes' >> ~/.asdfrc
+echo 'export EDITOR="code --wait"' >> ~/.bashrc
 
-# Install ruby and nodejs
-asdf install ruby 3.3.0
-asdf global ruby 3.3.0
-asdf install nodejs 20.11.0
-asdf global nodejs 20.11.0
+# Installl Opera
+wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+echo "deb https://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list
+sudo apt-get update && sudo apt-get install opera-stable
 
-# Install rails and yarn
-gem update --system
-npm install -g yarn
-gem install rails -v 7.1.3.2
+# install code
+sudo apt-get install code
+
+# install flameshot
+sudo apt install flameshot
+
+# install openfortiGUI
+wget https://apt.iteas.at/iteas/pool/main/o/openfortigui/openfortigui_0.9.10-1_amd64_noble.deb -O openfortigui.deb
+sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qt5keychain-dev libqt5core5a libqt5gui5 libqt5keychain1 libqt5network5 libqt5widgets5 qttranslations5-l10n ppp
+sudo apt-get install -f
+sudo dpkg -i openfortigui.deb
+
+# install chromium
+sudo apt install chromium-chromedriver
+
+# install chrome 117
+wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_117.0.5938.132-1_amd64.deb
+sudo apt-get install libasound2t64
+sudo apt-get install fonts-liberation libasound2 libnspr4 libnss3 libu2f-udev xdg-utils
+#sudo apt-get install -f
+sudo dpkg -i google-chrome-stable_117.0.5938.132-1_amd64.deb
+sudo apt-mark hold google-chrome-stable # prevent auto update
+
+# install notion - NEED TO FIX  
+# sudo apt install liblua5.3-0
+# wget http://archive.ubuntu.com/ubuntu/pool/multiverse/n/notion/notion_4.0.2+dfsg-7build1_amd64.deb -O notion.deb
+# sudo apt install ./notion.deb
+
+# install discord
+wget "https://discordapp.com/api/download?platform=linux&format=deb" -O discord.deb 
+sudo dpkg -i discord.deb
