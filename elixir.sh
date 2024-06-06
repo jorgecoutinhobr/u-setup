7d20d3282557e1d64f0f49b05a37b1be78ac6e57
Jorge Coutinho <jorgecsn@id.uff.br>
2024-06-06 11:39:11 -0300

    fix elixir

--- elixir.sh
+++ elixir.sh
@@ -1,12 +1,12 @@
 # install erlang
-asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin add erlang 
 asdf install erlang 24.0
 asdf global erlang 24.0
 
 # install elixir
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-asdf 1.16.0-otp-26
-asdf global elixir 1.16.0-otp-26
+asdf plugin-add elixir
+asdf install elixir 1.16.0
+asdf global elixir 1.16.0
 
 # install phoenix
 mix local.hex --force
