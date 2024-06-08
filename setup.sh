9f7147dd2422acabbef7b95e6e613e9a8a53ffc8
jorge coutinho <102194363+jorgecoutinhobr@users.noreply.github.com>
2024-06-08 12:21:45 -0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# basics tools
 sudo apt-get install -y git curl unzip
 
 for software in ./software/*.sh; do source $software; done
 for config in ./config/*.sh; do source $config; done
 
-# Add interactivity to the script
-gum confirm "Ready to logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
+gum confirm "Logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
