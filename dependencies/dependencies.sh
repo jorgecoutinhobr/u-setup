e49e60aacd75ab0cdec3028acc4bd5a1565e8e1c
Jorge Coutinho <jorgecsn@id.uff.br>
2024-06-07 22:39:33 -0300

    add curl to dependencies

--- dependencies/dependencies.sh
+++ dependencies/dependencies.sh
@@ -1,6 +1,6 @@
 # install dependencies
 sudo apt-get update
-sudo apt-get install -y git-core libnotify4 libxss1 fonts-liberation xdg-utils zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev autoconf m4 libncurses5-dev libwxgtk3.2-dev libwxgtk-webview3.2-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk unzip 
+sudo apt-get install -y git-core curl libnotify4 libxss1 fonts-liberation xdg-utils zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev autoconf m4 libncurses5-dev libwxgtk3.2-dev libwxgtk-webview3.2-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk unzip 
 
 # install asdf
 git clone https://github.com/excid3/asdf.git ~/.asdf
