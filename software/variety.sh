06b9e6c5ae9a2ef6dd66bad8e632ecd13fd7a306
jorge coutinho <102194363+jorgecoutinhobr@users.noreply.github.com>
2024-06-09 19:05:04 -0300

    add variety and wallpaper

---
+++ software/variety.sh
@@ -0,0 +1,6 @@
+sudo apt install variety
+
+sleep 2
+
+mv ~/.local/share/u-setup/wallpaper/black-wolf.jpg ~/.config/variety/Favorites
+mv ~/.local/share/u-setup/wallpaper/pixel-art.jpg ~/.config/variety/Favorites
