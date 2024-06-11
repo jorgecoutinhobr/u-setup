ed265daf0bd12c8b6a5ff6a1fd620711409d88eb
Jorge Coutinho <jorgecsn@id.uff.br>
2024-06-11 11:06:49 -0300

    fix

--- software/ulauncher.sh
+++ software/ulauncher.sh
@@ -5,6 +5,6 @@ sudo apt install -y ulauncher
 
 mkdir -p ~/.config/autostart/
 cp ~/.local/share/u-setup/config/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
-gtk-launch ulauncher.desktop >/dev/null
+gtk-launch ulauncher.desktop >/dev/null 2>&1
 sleep 4
 cp ~/.local/share/u-setup/config/ulauncher.json ~/.config/ulauncher/settings.json
