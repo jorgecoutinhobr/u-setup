3e87e16ccf6a1a46e5a614fb8a3846faaa6d7b1b
jorge coutinho <102194363+jorgecoutinhobr@users.noreply.github.com>
2024-06-15 11:51:55 -0300

    add opera from snap

--- software/opera.sh
+++ software/opera.sh
@@ -1,3 +1,1 @@
-wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
-echo "deb https://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list
-sudo apt-get update && sudo apt-get install opera-stable
+sudo snap install opera
