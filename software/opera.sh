3bf2d6ee94f5a2836481beb29d0114339d8c152a
Jorge Coutinho <jorgecsn@id.uff.br>
2024-07-12 17:12:35 -0300

    add opera from snap and rubymine

--- software/opera.sh
+++ software/opera.sh
@@ -1,3 +1,6 @@
-wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
-echo "deb https://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list
-sudo apt-get update && sudo apt-get install opera-stable
+# wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+# echo "deb https://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list
+# sudo apt-get update && sudo apt-get install opera-stable
+
+# opera from snap does not need config to run livestreams and videos
+sudo snap install opera
