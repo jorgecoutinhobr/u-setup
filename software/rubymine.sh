b09f227ee45d54169a3bb0de9946e8b9dc54e208
Jorge Coutinho <jorgecsn@id.uff.br>
2024-07-12 20:15:45 -0300

    teste toolbox

--- software/rubymine.sh
+++ software/rubymine.sh
@@ -1,1 +1,6 @@
-sudo snap install rubymine --classic
+cd ~/Downloads
+sudo apt-get install -y libfuse2
+wget -O toolbox.tar.gz 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.4.0.32175.tar.gz'
+sudo tar xzf toolbox.tar.gz -C /opt/
+rm toolbox.tar.gz
+cd -
